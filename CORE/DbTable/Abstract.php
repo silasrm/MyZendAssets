322c9e4ffea4684ba9a8654de43729c4f50652c6
Andre Santos <andre@dexdev.com.br>
2013-02-27 13:45:54 -0300

    Adicionado propriedade para informar a convenção de nomenclatura das tabelas UPPERCASE / lowercase

--- CORE/DbTable/Abstract.php
+++ CORE/DbTable/Abstract.php
@@ -2,6 +2,22 @@
 
 abstract class CORE_DbTable_Abstract extends Zend_Db_Table_Abstract
 {
+
+    protected $_conventionTableName = 'upper';
+
+    protected function _setupTableName() {
+        switch ($this->_conventionTableName) {
+            case 'lower':
+                $this->_name = strtolower($this->_name);
+                break;
+            case 'upper':
+                $this->_name = strtoupper($this->_name);
+                break;
+        }
+        parent::_setupTableName();
+    }
+	
+	
 	public function getName()
 	{
 		return $this->_name;
