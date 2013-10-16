07a48281f44bd83d33fd7f2cdfb6196ad52f7837
Silas Ribas <silasrm@gmail.com>
2013-10-15 23:21:33 -0300

    Adicionado suporte a estrutra stacked. Refatorado código de checagem de subpagina ativa.

--- CORE/View/Helper/MenuBootstrap.php
+++ CORE/View/Helper/MenuBootstrap.php
@@ -18,6 +18,10 @@ class CORE_View_Helper_MenuBootstrap
         {
             return $this->_navList( $navigation, $cssClass );
         }
+        else if( $type == 'stacked' )
+        {
+            return $this->_stacked( $navigation, $cssClass );
+        }
 
         return $this->_normal( $navigation, $cssClass );
     }
@@ -60,13 +64,7 @@ class CORE_View_Helper_MenuBootstrap
                     $algumAtivo = true;
                 }
 
-                foreach( $subpage->pages as $ssubpage )
-                {
-                    if( $ssubpage->isActive() )
-                    {
-                        $algumAtivo = true;
-                    }
-                }
+                $algumAtivo = $this->checaAtivo($subpage);
 
                 // visibility of the sub-page
                 if (!$subpage->isVisible()) {
@@ -174,13 +172,7 @@ class CORE_View_Helper_MenuBootstrap
                     $algumAtivo = true;
                 }
 
-                foreach( $subpage->pages as $ssubpage )
-                {
-                    if( $ssubpage->isActive() )
-                    {
-                        $algumAtivo = true;
-                    }
-                }
+                $algumAtivo = $this->checaAtivo($subpage);
 
                 // visibility of the sub-page
                 if (!$subpage->isVisible()) {
@@ -257,5 +249,135 @@ class CORE_View_Helper_MenuBootstrap
         $html[] = '</ul>';
 
         return join(PHP_EOL, $html);
+    }
+
+    protected function _stacked($navigation, $cssClass = null)
+    {
+        $html = array('<ul class="nav ' . $cssClass . '">');
+
+        foreach($navigation->getContainer() as $page)
+        {
+            // visibility of the page
+            if (!$page->isVisible()) {
+                continue;
+            }
+
+            // dropdown
+            $dropdown = !empty($page->pages);
+
+            $liClass = array();
+
+            if( $page->isActive() )
+            {
+                $liClass[] = 'active';
+            }
+
+            if( $dropdown )
+            {
+                $liClass[] = '';
+            }
+
+            $htmlSubpaginas = array('<ul class="nav nav-stacked">');
+
+            $countInativas = 0;
+            $algumAtivo = false;
+            $existeSubmenu = false;
+            foreach( $page->pages as $subpage )
+            {
+                if( $subpage->isActive() )
+                {
+                    $algumAtivo = true;
+
+                    $htmlSubpaginas[0] = '<ul class="nav nav-stacked in">';
+                }
+
+                $algumAtivo = $this->checaAtivo($subpage);
+
+                // visibility of the sub-page
+                if (!$subpage->isVisible()) {
+                    $countInativas++;
+                    continue;
+                }
+
+                if( !Zend_Registry::get('acl')->isAllowed( Zend_Registry::get('role'), $subpage->getResource(), $subpage->getPrivilege() ) ) {
+                    $countInativas++;
+                    continue;
+                }
+                else
+                {
+                    $existeSubmenu = true;
+                }
+
+                $htmlSubpaginas[] = '<li' . ($subpage->isActive() ? ' class="active"' : '') . '>';
+                $htmlSubpaginas[] = '<a href="' . $subpage->getHref() . '">';
+
+                if ($subpage->get('icon')) {
+                    $htmlSubpaginas[] = '<i class="icon-' . $subpage->get('icon') . '"></i>';
+                }
+
+                $htmlSubpaginas[] = $subpage->getLabel();
+                $htmlSubpaginas[] = "</a>";
+                $htmlSubpaginas[] = "</li>";
+            }
+
+            $htmlSubpaginas[] = "</ul>";
+
+            if( !$existeSubmenu && !Zend_Registry::get('acl')->isAllowed( Zend_Registry::get('role'), $page->getResource(), $page->getPrivilege() ) ) {
+                $countInativas++;
+                continue;
+            }
+
+            if( count($page->pages) == $countInativas )
+            {
+                $dropdown = false;
+                $htmlSubpaginas = array();
+            }
+
+            if( $algumAtivo )
+            {
+                $liClass[] = 'active';
+            }
+
+            // header
+            $html[] = '<li' . (( count($liClass) > 0 ) ? ' class="' . implode(' ', $liClass) . '"' : '') . '>';
+            $htmlPagina = array('<a href="' . ($dropdown ? '#' : $page->getHref()) . '" '
+                    . ($dropdown ? 'class="dropdown-collapse"': null)
+                    . '>');
+            $htmlPagina[] = '<span>' . $page->getLabel() . '</span>';
+
+            if ($dropdown) {
+                $htmlPagina[] = "<i class='icon-angle-down angle-down'></i>";
+            }
+
+            $htmlPagina[] = '</a>';
+
+            $html = array_merge( $html, $htmlPagina );
+            $html = array_merge( $html, $htmlSubpaginas );
+
+            $html[] = "</li>";
+        }
+
+        $html[] = '</ul>';
+
+        return join(PHP_EOL, $html);
+    }
+
+    public function checaAtivo($subpage)
+    {
+        $algumAtivo = false;
+
+        if(!empty($subpage->pages))
+        {
+            foreach( $subpage->pages as $ssubpage )
+            {
+                $algumAtivo = $this->checaAtivo($ssubpage);
+            }
+        }
+        else
+        {
+            $algumAtivo = $subpage->isActive();
+        }
+
+        return $algumAtivo;
     }
 }
